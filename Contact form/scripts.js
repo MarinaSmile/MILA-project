acf8af198f9682ed48b8dfab3876984eb9f58ee6
akolliakou <anna.kolliakou@kcl.ac.uk>
2019-03-05 20:24:08 +0000

    Revert "Merge branch 'master' into more-main-page-changes"
    This reverts commit 99a41b122d7bfb7b0691c966a9eac2b53ff975b9, reversing
    changes made to 4198eddaf952420ab6d46bb48b32611fc7cf9c0f.

--- Contact form/scripts.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-  $('.btn').on('mouseenter', function () {
-    $(this).css("background", "yellowgreen");
-  });
-  $('.btn').on('mouseleave', function () {
-    $(this).css("background", "green");
-  });
-});
