de8ac6657009934b3de0d59970bd658e1c2291c2
akolliakou <anna.kolliakou@kcl.ac.uk>
2019-03-05 20:39:08 +0000

    Revert "Merge branch 'master' of https://github.com/MarinaSmile/MILA-project"
    This reverts commit d3d363a273291a3c78cc191adc9414507813249d, reversing
    changes made to 4de02a477dc6c3c61b6b20615d187eb9816affdd.

--- Green Stores/scripts.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-  $('#load').on('mouseenter', function () {
-    $(this).css("background", "yellowgreen");
-  });
-  $('#load').on('mouseleave', function () {
-    $(this).css("background", "green");
-  });
-});
